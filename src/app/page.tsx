84d5c3ca50bbe8ce41c490eed707d20f568d54a9
Kashifzaii <kashifzai125@gmail.com>
2025-01-22 03:09:24 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+
+import Hero from "@/Components/Hero";
+import Features from "@/Components/Features"
+import Ceramics from "@/Components/Ceramics";
+import Popular from "@/Components/popular";
+import News from "@/Components/Newsletter"
+import Bottom from "@/Components/Features2"
+import Header2 from "@/Components/Header2";
+
+
+
+
+
+
+export default function Home() {
+  return (
+    <>
+    <Header2 />
+    <Hero />
+    <Features />
+    <Ceramics />
+    <Popular />
+    <News />
+    <Bottom />
+    </>
+    
+    
+  
+    
+  );
+}
